af12b6a861ce31cf65944162334a5752745ac86d
valoon3 <valoon2@naver.com>
2024-07-28 16:02:50 +0900

    [feat] prettierrc eslint setting

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/api')
+  getMain(): string {
+    return 'aaaa';
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT;
 
+  app.enableCors();
+
   await app.listen(port);
 }
 bootstrap();
