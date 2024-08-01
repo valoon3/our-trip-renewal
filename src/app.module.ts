4204a68dc5cc11fea32a00626425b4c9398b5509
valoon3 <valoon2@naver.com>
2024-08-02 00:48:10 +0900

    [feat] user router 설정 / 디비 연결
    1. 디비 연결 확인
    2. user resouce 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerMiddleware } from '../middleware/logger/logger.middleware';
 import { HttpExceptionFilter } from '../filter/http-exception.filter';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -11,6 +13,21 @@ import { HttpExceptionFilter } from '../filter/http-exception.filter';
       isGlobal: true,
       envFilePath: [`.env.${process.env.NODE_ENV}`],
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService): Promise<TypeOrmModuleOptions> => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService, { provide: 'APP_FILTER', useClass: HttpExceptionFilter }],
---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,5 @@
+export class User {
+  id: number;
+  userId: string;
+  password: string;
+}
